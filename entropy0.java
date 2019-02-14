ad43c00d5dde12591dbe706dde61340347653537
JetErorr <jardeshna@gmail.com>
2019-02-14 09:51:39 +0530

    lz77

--- entropy0.java
+++ entropy0.java
@@ -3,8 +3,8 @@ import java.util.*;
 import java.lang.*;
 
 class entropy0{
-    // static final int MAX_CHAR = 256; 
-    public static void main (String []args) 
+    // static final int MAX_CHAR = 256;
+    public static void main (String []args)
     {
         float sum=0;
         Scanner sc = new Scanner(System.in);
@@ -12,7 +12,7 @@ class entropy0{
         String word = sc.nextLine();
         int n = word.length();
         float p[] = new float[n];
-        char ch[] = new char[n]; 
+        char ch[] = new char[n];
         int count[] = new int[5];
         for (int i = 0; i < n; i++)
             {
